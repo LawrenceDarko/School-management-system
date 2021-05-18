7aec3d0e44e989d2842af1e3411ed300a8bee097
LawrenceDarko <darkolawrence@gmail.com>
2021-05-18 18:15:50 +0800

    New models

--- educore/models.py
+++ educore/models.py
@@ -3,17 +3,18 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Home(models.Model):
     welcome_head = models.CharField(max_length=150)
-    text_summary = models.TextField(max_length=500)
+    text_summary = models.TextField(max_length=700)
     image = models.ImageField(upload_to='WelcomeImage/', blank=True, null=True)
-
 
     def __str__(self):
         return self.welcome_head
 
     def snippet(self):
         return self.text_summary[:20]+'...'
+
 
 class Course(models.Model):
     course_title = models.CharField(max_length=150)
@@ -22,6 +23,7 @@ class Course(models.Model):
     def __str__(self):
         return self.course_title
 
+
 class NoticeImage(models.Model):
     image_title = models.CharField(max_length=15)
     image = models.ImageField(upload_to='NoticeImage/', blank=True, null=True)
@@ -29,19 +31,20 @@ class NoticeImage(models.Model):
     def __str__(self):
         return self.image_title
 
+
 class Notice(models.Model):
-    event_name = models.CharField(max_length=30)
-    event_details = models.TextField(max_length=100)
+    event_name = models.CharField(max_length=100)
+    event_details = models.TextField(max_length=200)
 
     def __str__(self):
         return self.event_name
-    
+
 
 # class Admission(models.Model):
 #     admission_essay = models.TextField(max_length=300)
 
 class AdmitNotice(models.Model):
-    admit_essay = models.TextField(max_length=500)
+    admit_essay = models.TextField(max_length=600)
 
     def __str__(self):
         return self.admit_essay
@@ -58,18 +61,22 @@ class ResearchInfo(models.Model):
 #     title = models.CharField(max_length=20)
 #     research_essay = models.TextField(max_length=300)
 
+
 class StudentNews(models.Model):
-    news_title = models.CharField(max_length =20)
+    news_title = models.CharField(max_length=50)
     news_description = models.TextField(max_length=200)
-    news_image = models.ImageField(upload_to='NewsImages/', blank=True, null=True)
+    news_image = models.ImageField(
+        upload_to='NewsImages/', blank=True, null=True)
 
     def __str__(self):
         return self.news_title
 
+
 class StudentNewsTwo(models.Model):
-    news_title = models.CharField(max_length =20)
+    news_title = models.CharField(max_length=50)
     news_description = models.TextField(max_length=200)
-    news_image = models.ImageField(upload_to='NewsImages/', blank=True, null=True)
+    news_image = models.ImageField(
+        upload_to='NewsImages/', blank=True, null=True)
 
     def __str__(self):
         return self.news_title
@@ -79,33 +86,41 @@ class StudentNewsTwo(models.Model):
 #     description = models.TextField(max_length=40)
 #     image = models.ImageField(upload_to='NewsImages/', blank=True, null=True)
 
+
 class Footer(models.Model):
-    address = models.CharField(max_length=30, null=True)
-    location = models.CharField(max_length=30)
-    telephone = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
+    address = models.CharField(max_length=100, null=True)
+    location = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
 
     def __str__(self):
         return self.location
 
+
 class SchoolGallery(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField(max_length=500)
-    image = models.ImageField(upload_to='SchoolGallery/', blank=True, null=True)
+    image = models.ImageField(
+        upload_to='SchoolGallery/', blank=True, null=True)
 
 
 class SchoolHistory(models.Model):
-    proprietor_history = models.TextField(max_length=600)
-    proprietor_image = models.ImageField(upload_to='StaffImages/', blank=True, null=True)
+    proprietor_history = models.TextField(max_length=700)
+    proprietor_image = models.ImageField(
+        upload_to='StaffImages/', blank=True, null=True)
+
 
 class AdministratorHistory(models.Model):
-    administrator_history = models.TextField(max_length=600)
-    administrator_image = models.ImageField(upload_to='StaffImages/', blank=True, null=True)
+    administrator_history = models.TextField(max_length=1000)
+    administrator_image = models.ImageField(
+        upload_to='StaffImages/', blank=True, null=True)
+
 
 class StaffInfo(models.Model):
     staff_name = models.CharField(max_length=50)
     staff_description = models.TextField(max_length=100)
-    staff_image = models.ImageField(upload_to='StaffImages/', blank=True, null=True)
+    staff_image = models.ImageField(
+        upload_to='StaffImages/', blank=True, null=True)
 
     def __str__(self):
         return self.staff_name
